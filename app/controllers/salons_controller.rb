84492ea8c1dc5941de31963e83f12b94674656fc
yusuke0024 <scattered213@gmail.com>
2020-11-24 08:46:50 +0900

    サロン情報登録失敗時エラーメッセージ追加

--- app/controllers/salons_controller.rb
+++ app/controllers/salons_controller.rb
@@ -1,7 +1,20 @@
 class SalonsController < ApplicationController
   def new
+    @salon = Salon.new
   end
 
   def create
+    @salon = Salon.new(salon_params)
+    if @salon.save
+      
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def salon_params
+    params.require(:salon).permit(:name, :adress)
   end
 end
--- app/models/salon.rb
+++ app/models/salon.rb
@@ -1,2 +1,4 @@
 class Salon < ApplicationRecord
+  validates :name,   presence: true, length: { maximum: 20 }
+  validates :adress, presence: true, length: { maximum: 150 }
 end
