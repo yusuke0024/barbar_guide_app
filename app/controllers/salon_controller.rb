8629155150a24518c19bb980d30eee5cb553903f
yusuke0024 <scattered213@gmail.com>
2020-11-23 19:37:24 +0900

    サロン新規登録画面表示

---
+++ app/controllers/salon_controller.rb
@@ -0,0 +1,4 @@
+class SalonController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/", to: "static_pages#home"
+  resources :salon, only:[:new]
 end
