2904c644d0960cfd7d41f39f8b00f54e1f0090f6
yusuke0024 <scattered213@gmail.com>
2020-11-24 09:36:59 +0900

    validatesのスペースを揃える

--- app/models/salon.rb
+++ app/models/salon.rb
@@ -1,4 +1,4 @@
 class Salon < ApplicationRecord
-  validates :name,   presence: true, length: { maximum: 20 }
+  validates :name,    presence: true, length: { maximum: 20 }
   validates :address, presence: true, length: { maximum: 150 }
 end
