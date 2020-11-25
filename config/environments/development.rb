9c2978d8406c537023e2181a80b8a6a8776398be
yusuke0024 <scattered213@gmail.com>
2020-11-25 16:57:21 +0900

    環境変数指定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,5 +57,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+  config.logger = Logger.new STDOUT
+
   config.file_watcher = ActiveSupport::FileUpdateChecker
 end
