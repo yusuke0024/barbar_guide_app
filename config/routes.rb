881a411a14ffc4a5481fa61ca151422b551ef2bc
yusuke0024 <scattered213@gmail.com>
2020-11-26 08:10:04 +0900

    ユーザー登録ページ表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/", to: "static_pages#home"
   resources :salons, only: [:index, :new, :create]
+  resources :users, only: [:new]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_104612) do
+ActiveRecord::Schema.define(version: 2020_11_25_090919) do
 
   create_table "salons", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
