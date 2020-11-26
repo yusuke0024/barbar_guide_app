ad3b9021659c513e9af9fc32d698b30ad5029bf4
yusuke0024 <scattered213@gmail.com>
2020-11-26 11:19:56 +0900

    ユーザー登録機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to salons_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
