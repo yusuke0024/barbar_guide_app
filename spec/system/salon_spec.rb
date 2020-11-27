a37e89d35dd155f97a8f1ec6529ce0c9cd4c7f8e
yusuke0024 <scattered213@gmail.com>
2020-11-27 11:19:53 +0900

    サロン名２０文字以上だった場合にサロン登録失敗するテスト追加

--- spec/system/salon_spec.rb
+++ spec/system/salon_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe "Salon", type: :system do
       expect(page).to have_content "サロン名を入力してください"
     end
   end
+
+  context "サロン名が２０文字以上のとき" do
+    let(:name) { "宇宙で一番名前が長いのでどんな名前やねんそれ、ほんまにヘアサロンか？とツッコミがくるお店" }
+
+    it "サロン登録に失敗すること" do
+      visit new_salon_path
+      fill_in "salon_name", with: name
+      fill_in "salon_address", with: "東京都千代田区丸の内1-8-1"
+      click_on "登録"
+
+      expect(page).to have_content "サロン名は20文字以内で入力してください"
+    end
+  end
 end
