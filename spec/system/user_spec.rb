406aa505e34995ad8b6e51d0b353b9702cf5522e
yusuke0024 <scattered213@gmail.com>
2020-11-27 09:34:23 +0900

    ユーザー名５０文字以上の時の登録失敗テスト追加

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -15,4 +15,18 @@ RSpec.describe "User", type: :system do
       expect(page).to have_content "Name can't be blank"
     end
   end
+  
+  context "ユーザー名が５０文字以上の時" do
+    let(:name) { "じゅげむじゅげむごこうのすりきれかいじゃりすいぎょのすいぎょうまつうんらいまつふうらいまつくうねるところにすむところやぶらこうじのぶらこうじ" }
+
+    it "ユーザー登録に失敗すること" do
+      visit new_user_path
+      fill_in "user_name", with: name
+      fill_in "user_email", with: "example@mail.com"
+      fill_in "user_password", with: "foobar"
+      click_on "登録"
+
+      expect(page).to have_content "Name is too long (maximum is 50 characters)"
+    end
+  end
 end
