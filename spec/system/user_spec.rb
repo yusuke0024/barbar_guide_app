572018ce6b19b8ef94d184e7e3caf0a282715147
yusuke0024 <scattered213@gmail.com>
2020-11-28 11:02:50 +0900

    describeでユーザー登録機能を一まとめにする

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,32 +1,34 @@
 require "rails_helper"
 
 RSpec.describe "User", type: :system do
+  describe "ユーザー登録機能" do
 
-  context "ユーザー名入力フォームが空白のとき" do
-    let(:name) { "" }
+    context "ユーザー名入力フォームが空白のとき" do
+      let(:name) { "" }
 
-    it "ユーザー登録に失敗すること" do
-      visit new_user_path
-      fill_in "user_name", with: name
-      fill_in "user_email", with: "example@mail.com"
-      fill_in "user_password", with: "foobar"
-      click_on "登録"
+      it "ユーザー登録に失敗すること" do
+        visit new_user_path
+        fill_in "user_name", with: name
+        fill_in "user_email", with: "example@mail.com"
+        fill_in "user_password", with: "foobar"
+        click_on "登録"
 
-      expect(page).to have_content "ユーザー名を入力してください"
+        expect(page).to have_content "ユーザー名を入力してください"
+      end
     end
-  end
-  
-  context "ユーザー名が５０文字以上の時" do
-    let(:name) { "じゅげむじゅげむごこうのすりきれかいじゃりすいぎょのすいぎょうまつうんらいまつふうらいまつくうねるところにすむところやぶらこうじのぶらこうじ" }
 
-    it "ユーザー登録に失敗すること" do
-      visit new_user_path
-      fill_in "user_name", with: name
-      fill_in "user_email", with: "example@mail.com"
-      fill_in "user_password", with: "foobar"
-      click_on "登録"
+    context "ユーザー名が５０文字以上の時" do
+      let(:name) { "じゅげむじゅげむごこうのすりきれかいじゃりすいぎょのすいぎょうまつうんらいまつふうらいまつくうねるところにすむところやぶらこうじのぶらこうじ" }
 
-      expect(page).to have_content "ユーザー名は50文字以内で入力してください"
+      it "ユーザー登録に失敗すること" do
+        visit new_user_path
+        fill_in "user_name", with: name
+        fill_in "user_email", with: "example@mail.com"
+        fill_in "user_password", with: "foobar"
+        click_on "登録"
+
+        expect(page).to have_content "ユーザー名は50文字以内で入力してください"
+      end
     end
   end
 end
