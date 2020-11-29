9d03ea490dc6f401417f9a72a98862cb4da5f5d2
yusuke0024 <scattered213@gmail.com>
2020-11-29 15:06:23 +0900

    レビュー投稿フォーム表示

--- app/controllers/salons/reviews_controller.rb
+++ app/controllers/salons/reviews_controller.rb
@@ -1,4 +1,10 @@
 class Salons::ReviewsController < ApplicationController
   def new
+    @review = Review.new
+    @salon = Salon.find(params[:salon_id])
+  end
+
+  def create
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "/", to: "static_pages#home"
   resources :salons, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:new], controller: "salons/reviews"
+    resources :reviews, only: [:new, :create], controller: "salons/reviews"
   end
   resources :users, only: [:new, :create]
   resource  :sessions, only: [:new, :create, :destroy]
