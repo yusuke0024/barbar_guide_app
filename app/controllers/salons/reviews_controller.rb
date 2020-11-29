ea42cd83ae264bc73563d3cfd0271359ed9d1aa4
yusuke0024 <scattered213@gmail.com>
2020-11-29 16:59:56 +0900

    レビューを投稿できるようにした

--- app/controllers/salons/reviews_controller.rb
+++ app/controllers/salons/reviews_controller.rb
@@ -5,6 +5,17 @@ class Salons::ReviewsController < ApplicationController
   end
 
   def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to salon_path(@review.salon)
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def review_params
+    params.permit(:salon_id, reviews: [:comment]).merge(user_id: current_user.id)
   end
 end
