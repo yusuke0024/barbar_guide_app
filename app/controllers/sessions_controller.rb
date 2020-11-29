eb56bac1551f3fd1703e3936164f7bfd9470d23a
yusuke0024 <scattered213@gmail.com>
2020-11-29 11:03:57 +0900

    flashが出るようにした

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
 
     unless user&.authenticate(params[:password])
+      flash.now[:alert] = "IDかパスワードが間違っているか、会員ではありません。"
       return render :new
     end
 
     session[:user_id] = user.id
-    redirect_to salons_path
+    redirect_to salons_path, notice: "ログインしました"
   end
 
   def destroy
     session.delete(:user_id)
-    redirect_to salons_path
+    redirect_to salons_path, notice: "ログアウトしました"
   end
 end
