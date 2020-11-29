1bd66540e84cc03ffd49dbafa962c84a8191fdbe
yusuke0024 <scattered213@gmail.com>
2020-11-29 14:01:14 +0900

    インデックス生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_044452) do
+ActiveRecord::Schema.define(version: 2020_11_29_045637) do
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_11_29_044452) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["salon_id", "user_id"], name: "index_reviews_on_salon_id_and_user_id"
   end
 
   create_table "salons", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
