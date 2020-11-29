e13b05d337edcaaeab25ad610fbf210d74a67503
yusuke0024 <scattered213@gmail.com>
2020-11-29 12:55:13 +0900

    vaild_loginメソッド部分にコメント追記

--- spec/system/login_spec.rb
+++ spec/system/login_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature "Login", type: :system do
 
   context "有効な情報を入力したとき" do
     it "ユーザーはログインに成功すること" do
+      #有効なデータでログイン
       valid_login(user)
       expect(current_path).to eq salons_path
       expect(page).to have_content "ログインしました"
