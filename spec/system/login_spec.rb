ce99e91cd6dc63fab517ea4923c445aa26987028
yusuke0024 <scattered213@gmail.com>
2020-11-29 12:42:50 +0900

    ダブルクォーテンションに変更

--- spec/system/login_spec.rb
+++ spec/system/login_spec.rb
@@ -1,14 +1,14 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature 'Login', type: :system do
+RSpec.feature "Login", type: :system do
   let(:user) { FactoryBot.create(:user) }
 
-  context '有効な情報を入力したとき' do
-    it 'ユーザーはログインに成功すること' do
+  context "有効な情報を入力したとき" do
+    it "ユーザーはログインに成功すること" do
       valid_login(user)
       expect(current_path).to eq salons_path
-      expect(page).to have_content 'ログインしました'
-      expect(page).to have_content 'サロン一覧表示'
+      expect(page).to have_content "ログインしました"
+      expect(page).to have_content "サロン一覧表示"
     end
   end
 end
