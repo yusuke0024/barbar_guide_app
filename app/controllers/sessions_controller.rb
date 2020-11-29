c79d964994fe475d7d54ddb1ae57d1c3c3f1690e
yusuke0024 <scattered213@gmail.com>
2020-11-29 10:04:01 +0900

    ログインできるようにした

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    unless user&.authenticate(params[:password])
+      return render :new
+    end
+
+    session[:user_id] = user.id
+    redirect_to salons_path
+  end
 end
