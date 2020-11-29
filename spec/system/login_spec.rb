70cb2afdff427bcf5151b5397b8fcb0fb78f8f08
yusuke0024 <scattered213@gmail.com>
2020-11-29 12:06:03 +0900

    ログインテスト追加

---
+++ spec/system/login_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature 'Login', type: :system do
+  let(:user) { FactoryBot.create(:user) }
+
+  context '有効な情報を入力したとき' do
+    it 'ユーザーはログインに成功すること' do
+      valid_login(user)
+      expect(current_path).to eq salons_path
+      expect(page).to have_content 'ログインしました'
+      expect(page).to have_content 'サロン一覧表示'
+    end
+  end
+end
