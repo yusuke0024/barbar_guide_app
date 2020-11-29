1937f4f8c62573f4e3ecc27b5cab5543de0bf588
yusuke0024 <scattered213@gmail.com>
2020-11-29 11:11:52 +0900

    カレントユーザーを設定するメソッド追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :danger
-  helper_method :logged_in?
+  helper_method :logged_in?, :current_user
 
   def logged_in?
     !!session[:user_id]
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
   end
 end
