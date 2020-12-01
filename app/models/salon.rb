74e96f46fd781093ec87ecbd8422365094de7f4a
yusuke0024 <scattered213@gmail.com>
2020-12-01 18:04:07 +0900

    アップローダーをサロンモデルにマウント

--- app/models/salon.rb
+++ app/models/salon.rb
@@ -2,4 +2,6 @@ class Salon < ApplicationRecord
   validates :name,    presence: true, length: { maximum: 20 }
   validates :address, presence: true, length: { maximum: 150 }
   has_many :reviews
+
+  mount_uploader :img, ImagesUploader
 end
