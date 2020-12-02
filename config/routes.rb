29b228897148744f4ef59cf42b37ed007d66ccda
yusuke0024 <scattered213@gmail.com>
2020-12-02 10:12:19 +0900

    rootにhomeを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/", to: "static_pages#home"
+  root "static_pages#home"
   resources :salons, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create], controller: "salons/reviews"
   end
